6805ffb211b99492f7f9893f9789aa3b1417b3a0
Thiago Oshiro <oshirothiago@gmail.com>
2022-08-06 17:18:28 -0300

    feat: New gitignore file, TrackingModel implementation

--- wallapp/accounts/models.py
+++ wallapp/accounts/models.py
@@ -1,3 +1,11 @@
+from xmlrpc.client import DateTime
 from django.db import models
 
-# Create your models here.
+
+class TrackingModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('created_at')
